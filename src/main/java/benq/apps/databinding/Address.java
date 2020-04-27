1090a98502736903f5867601be5cb194dc8f05bc
Ghebrehiywet Fissaha <g.hiwetmit@gmail.com>
2020-04-27 16:41:17 -0500

    initial commit

--- src/main/java/benq/apps/databinding/Address.java
+++ src/main/java/benq/apps/databinding/Address.java
@@ -1,4 +1,30 @@
 package benq.apps.databinding;
 
 public class Address {
+    private  String street;
+    private  String zipCode;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "street='" + street + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                '}';
+    }
 }
--- src/main/java/benq/apps/databinding/UserController.java
+++ src/main/java/benq/apps/databinding/UserController.java
@@ -1,4 +1,27 @@
 package benq.apps.databinding;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class UserController {
+    public UserController() {
+    }
+
+    @GetMapping("/signup")
+    public String createUser(Model model) {
+        model.addAttribute("model", new UserModel());
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String createUser(UserModel userModel) {
+
+        System.out.println(userModel);
+        return "signup";
+    }
+
 }
