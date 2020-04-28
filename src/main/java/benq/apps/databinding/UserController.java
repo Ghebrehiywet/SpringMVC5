f786c6848b2195d06f08ab5fcd7ea7451c5b0562
Ghebrehiywet Fissaha <g.hiwetmit@gmail.com>
2020-04-27 21:40:18 -0500

    ModelAttribute declared on two places, ref user controller

--- src/main/java/benq/apps/databinding/UserController.java
+++ src/main/java/benq/apps/databinding/UserController.java
@@ -4,6 +4,7 @@ package benq.apps.databinding;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -12,8 +13,14 @@ public class UserController {
     }
 
     @GetMapping("/signup")
-    public String createUser(Model model) {
-        model.addAttribute("model", new UserModel());
+    public String createUser(@ModelAttribute("User") UserModel userModel,
+                             @ModelAttribute("ModelMethod") UserModel mod2, Model model) {
+
+        //  Both modelMethod1 and modelMethod2 will have the same value
+        Object modelMethod1 = model.getAttribute("ModelMethod");
+        Object modelMethod2 = userModel;
+
+        System.out.println();
         return "signup";
     }
 
@@ -22,6 +29,13 @@ public class UserController {
 
         System.out.println(userModel);
         return "signup";
+    }
+
+    @ModelAttribute("ModelMethod")
+    public UserModel ModelMethod() {
+        UserModel userModel = new UserModel("1223", "Name");
+        System.out.println("Model Method:" + userModel);
+        return userModel;
     }
 
 }
